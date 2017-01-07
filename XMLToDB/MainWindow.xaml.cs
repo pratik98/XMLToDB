28abf0d9420d93478b338b665436188edd03a683
Pratik98 <pratik4fp@gmail.com>
2017-01-07 17:08:37 +0530

    added XMLProcessor Business Logic Class

--- XMLToDB/MainWindow.xaml.cs
+++ XMLToDB/MainWindow.xaml.cs
@@ -37,67 +37,34 @@ namespace XMLToDB
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             fbd.ShowDialog();
             logger = new FileLogger();
+            XMLProcessor xmlProObj = new XMLProcessor();
             StringBuilder jsonText = new StringBuilder();
             if(!String.IsNullOrWhiteSpace(fbd.SelectedPath))
-            {
-                var filePaths = getXmlFile(fbd.SelectedPath);
-                StringBuilder sb = new StringBuilder();
+            {                
+                var filePaths = xmlProObj.getXmlFiles(fbd.SelectedPath);
+
+                string xmlContent = string.Empty;
                  var json = new object();
-                          
+                      
                 foreach (var item in filePaths)
                 {
                     try
-                    {  
-                        sb.Append(File.ReadAllText(item.ToString()));
-                        json = new JavaScriptSerializer().Serialize(GetXmlData(XElement.Parse(sb.ToString())));
+                    {
+                        xmlContent = string.Empty;
+                        xmlContent=File.ReadAllText(item.ToString());
+
+                        json = new JavaScriptSerializer().Serialize(xmlProObj.GetXmlData(XElement.Parse(xmlContent)));
                     }
                     catch (Exception ex)
                      {
                         logger.log("\n"+ex.StackTrace.ToString());
                     }
+                    jsonText.AppendLine(json.ToString());
                 }
-                 jsonText.AppendLine( json.ToString());
+                
                }
             txtEditor.Text = jsonText.ToString();
         }
-        private List<String> getXmlFile(String Path)
-        {
-            
-             if (!String.IsNullOrWhiteSpace(Path))
-             {
-                 string[] files = Directory.GetFiles(Path);
-                 string[] folders = Directory.GetDirectories(Path);
-
-                 int fldrCount = folders.Length;
-                 int fileCount = files.Length;
-                 if(fldrCount ==0 && fileCount ==0)
-                 {
-                     return allPaths;
-                 }
-
-                 while (fldrCount > 0)
-                 {
-                     fldrCount--;
-                     //sb.AppendLine(getXmlFile(folders[fldrCount]));
-
-                    getXmlFile(folders[fldrCount]);
-                 }
-                 while(fileCount >0)
-                 {
-                     fileCount--;
-                   //  sb.AppendLine("Path:"+files[fileCount]);
-                     allPaths.Add( files[fileCount]);
-                                         
-                 }
-             }
-             return allPaths;
-        }
-        private static Dictionary<string, object> GetXmlData(XElement xml)
-        {
-            var attr = xml.Attributes().ToDictionary(d => d.Name.LocalName, d => (object)d.Value);
-            if (xml.HasElements) attr.Add("_value", xml.Elements().Select(e => GetXmlData(e)));
-            else if (!xml.IsEmpty) attr.Add("_value", xml.Value);
-             return new Dictionary<string, object> { { xml.Name.LocalName, attr } };
-        }
+               
     }
 }
---
+++ XMLToDB/codelibrary/XMLProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+namespace XMLToDB.codelibrary
+{
+    class XMLProcessor
+    {
+        List<string> allPaths = new List<string>();
+        ILogger logger;
+        public List<String> getXmlFiles(String Path)
+        {
+            if (!String.IsNullOrWhiteSpace(Path))
+            {
+                string[] files = Directory.GetFiles(Path);
+                string[] folders = Directory.GetDirectories(Path);
+
+                int fldrCount = folders.Length;
+                int fileCount = files.Length;
+                if (fldrCount == 0 && fileCount == 0)
+                {
+                    return allPaths;
+                }
+                            
+                while (fileCount > 0)
+                {
+                    fileCount--;
+                    if (files[fileCount].EndsWith(".xml") || files[fileCount].EndsWith(".xml.Meta"))
+                    {
+                        allPaths.Add(files[fileCount]);
+                    }
+                    
+                }
+                while (fldrCount > 0)
+                {
+                    fldrCount--;
+                    getXmlFiles(folders[fldrCount]);
+                }
+            }
+            return allPaths;
+        }
+        public  Dictionary<string, object> GetXmlData(XElement xml)
+        {
+            var attr = xml.Attributes().ToDictionary(d => d.Name.LocalName, d => (object)d.Value);
+            if (xml.HasElements) attr.Add("_value", xml.Elements().Select(e => GetXmlData(e)));
+            else if (!xml.IsEmpty) attr.Add("_value", xml.Value);
+            return new Dictionary<string, object> { { xml.Name.LocalName, attr } };
+        }
+        
+    }
+}
